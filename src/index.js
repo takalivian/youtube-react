3cde66b00a6455416fc2ad4ad8d8071646b6eba7
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-28 12:00:24 +0900

    npm-Install

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { StoreProvider } from './store/index';
+import { StoreProvider } from './store/index'
 
 ReactDOM.render(
   <React.StrictMode>
