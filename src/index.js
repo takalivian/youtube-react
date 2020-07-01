8eabb1d022db183b2bf788c443f49ad512385c75
takalivian <61145524+takalivian@users.noreply.github.com>
2020-07-02 00:58:21 +0900

    video表示

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import normalize from 'normalize.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StoreProvider } from './store/index'
